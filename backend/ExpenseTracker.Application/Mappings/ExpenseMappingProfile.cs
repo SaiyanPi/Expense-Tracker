d4891bc3e5a226cf716e87e4ddb208efd635f8b9
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2025-08-18 17:54:06 +0545

    updated expense mapping profile, added unit test for GetAllAsync method in expense service

--- backend/ExpenseTracker.Application/Mappings/ExpenseMappingProfile.cs
+++ backend/ExpenseTracker.Application/Mappings/ExpenseMappingProfile.cs
@@ -9,10 +9,15 @@ public class ExpenseMappingProfile : Profile
     public ExpenseMappingProfile()
     {
         // Create -> Entity
-        CreateMap<CreateExpenseDto, Expense>();
+        CreateMap<CreateExpenseDto, Expense>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())       // generated in code/db
+            .ForMember(dest => dest.Category, opt => opt.Ignore()); // nav property (EF will handle)
 
         // Update -> Entity
-        CreateMap<UpdateExpenseDto, Expense>();
+        CreateMap<UpdateExpenseDto, Expense>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())       // never update Id
+            .ForMember(dest => dest.UserId, opt => opt.Ignore())   // UserId itself can't be updat
+            .ForMember(dest => dest.Category, opt => opt.Ignore());
 
         // required to populate an edit form with existing Expense data
         CreateMap<Expense, UpdateExpenseDto>();
